a7c6c0515c7210400127218f551f9461dbd48b3b
Jimin Park <parkjimin0@gmail.com>
2018-07-31 12:05:03 -0400

    next

---
+++ bash.js
@@ -0,0 +1,13 @@
+process.stdout.write('prompt > ');
+process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim();
+    if (cmd === 'pwd') {
+        // console.log(`Current directory: ${process.cwd()}`);
+        process.stdout.write(`Current directory: ${process.cwd()}`)
+    } else {
+
+        process.stdout.write('You typed: ' + cmd);
+        process.stdout.write('\nprompt > ');
+    }
+
+});
